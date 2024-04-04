3e04eafb1db4e6355fa3a9e3cede1076144dca56
Berihun <taberihun07@gmail.com>
2024-04-04 15:06:58 +0300

    model modification

--- builder/models.py
+++ builder/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
-# Create your models here.
-
 class Page(models.Model):
     template = models.ForeignKey('Template', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.template.name} - {self.name}"
+
+class PageContent(models.Model):
+    page = models.OneToOneField(Page, on_delete=models.CASCADE)
     html = models.TextField()
     css = models.TextField()
     js = models.TextField()
 
     def __str__(self):
-        return f"{self.template.name} - {self.name}"
-    
+        return f"Content for {self.page.name}"
+
 class Template(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     preview_image = models.ImageField(upload_to="images/preview/")
-    html = models.TextField()
-    css = models.TextField()
-    js = models.TextField()
 
     def __str__(self):
         return self.name
--- builder/urls.py
+++ builder/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('getTemplate/', views.getTemplates, name="gettemplates"),
-    path('getTemplate/<int:pk>/', views.getTemplate, name="gettemplate")
+    path('getTemplate/<int:pk>/', views.getTemplate, name="gettemplate"),
+    path('getTemplatePages/<int:pk>/', views.getTemplatePages, name="gettemplatepage"),
+    path('updatePageContent/<int:pk>/', views.updatePageContent, name="updateTemplate")
 ]

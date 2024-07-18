06c1c74f22218b2464116a7dac798f3817fd1d07
esubalewsintie <esubalewsintie1302@gmail.com>
2024-07-19 01:29:44 +0300

    register merchant info

--- customer/models.py
+++ customer/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.conf import settings
+import uuid
+
 
 class Customer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField("Full name", max_length=1024)
-    address1 = models.CharField("Address line 1", max_length=1024)
+    first_name = models.CharField("First name", max_length=1024,blank=True, null=True)
+    last_name = models.CharField("Last name", max_length=1024,blank=True, null=True)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True) 
+    address1 = models.CharField("Address line 1", max_length=1024,blank=True, null=True)
     address2 = models.CharField("Address line 2", max_length=1024, blank=True, null=True)
-    zip_code = models.CharField("ZIP", max_length=12)
-    city = models.CharField("City", max_length=1024)
+    zip_code = models.CharField("ZIP", max_length=12,blank=True, null=True)
+    city = models.CharField("City", max_length=1024,blank=True, null=True)
     state = models.CharField("State", max_length=1024, blank=True, null=True)
-    country = models.CharField("Country", max_length=1024)
+    country = models.CharField("Country", max_length=1024,blank=True, null=True)
     phone_number = models.CharField("Phone Number", max_length=15, blank=True, null=True)
 
     def __str__(self):

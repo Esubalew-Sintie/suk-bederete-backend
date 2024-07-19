27528107d0e21d44d524e2deabe6c51a5e1965fe
esubalewsintie <esubalewsintie1302@gmail.com>
2024-07-19 14:00:29 +0300

    role based

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ import requests
 from django.http import JsonResponse
 
 # Create your views here.
+from account.models import Account
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
@@ -12,6 +13,8 @@ from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework_simplejwt.serializers import TokenVerifySerializer
+from django.contrib.auth.models import User
+
 # Create your views here.
 routes =  [
       {
@@ -31,33 +34,28 @@ routes =  [
 def getroutes(request):
     return Response(routes)
 
-
-
 class VerifyTokenView(APIView):
     def post(self, request, *args, **kwargs):
         token = request.data.get('token')
         refresh = request.data.get('refresh')
-        print(token['value'])
 
         try:
             # Manually verify the token
-            access_token = AccessToken(token['value'])
+            access_token = AccessToken(token)
             user_id = access_token['user_id']
-            print(user_id)
+            user = Account.objects.get(id=user_id)
+            role = user.role
 
-            return Response({'user_id': user_id})
+            return Response({'user_id': user_id, 'role': role})
         except TokenError as e:
-            if isinstance(e, InvalidToken) and refresh['value']:
+            if isinstance(e, InvalidToken) and refresh:
                 try:
-                    refresh_token = RefreshToken(refresh['value'])
+                    refresh_token = RefreshToken(refresh)
                     new_access_token = str(refresh_token.access_token)
                     return Response({'access': new_access_token})
                 except TokenError:
                     return Response({'error': 'Invalid refresh token'}, status=400)
             return Response({'error': str(e)}, status=400)
-        
-        
-
 
 def update_clerk_user_metadata(request, user_id, role):
     clerk_api_key = "sk_test_hqvJsTDARJZewKOibkM6auiWzDKDafHMMOVqAlgtHi"  # Replace with your actual Clerk API key
